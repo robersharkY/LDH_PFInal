ca08d391d81c3875272a1ee8fbf92b4ed4f2624e
Roberto Carlos García Cruz <alu0101036991@ull.edu.es>
2022-12-01 11:36:15 +0000

    ejemplo comprobado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,8 +37,6 @@ public class Main {
     quiz.printSimilarRatingsByGenreAndMinutes();
     System.out.println("---");
     quiz.printSimilarRatingsByYearAfterAndMinutes();
-
-    System.out.println("hola");
   }
 }
 
